cc29977c6f96a32135388b3a03c80b2c2cf3c34f
karynadauhulevich <karynadauhulevich@gmail.com>
2024-08-09 16:43:43 -0700

    2test

--- src/_test/userCreate.spec.js
+++ src/_test/userCreate.spec.js
@@ -1,13 +1,14 @@
 const request = require ('supertest')
-const {expect : ExpectStatic } = require('chai')
+const {expect} = require('chai')
 describe('USER CREATE' , () => {
     describe('USER CREATE - POSITIVE TESTS' , () => {
      it ('user create all fields', ()=>{
 
-
-
      })
 
+     it ('user create all fields', ()=>{
+
+     })
 
     })
 
