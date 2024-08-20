b7b55ae99aab627502458055ada85d0d4554fc24
karynadauhulevich <karynadauhulevich@gmail.com>
2024-08-19 22:36:56 -0700

    3 test creatir

--- src/_test/user/userGetById1.spec.js
+++ src/_test/user/userGetById1.spec.js
@@ -1,29 +1,46 @@
 const { expect } = require("chai");
-const { userGetByIdQ } = require('./queries');
+const { userGetByIdQ, userCreateQ} = require('./queries');
 const { user } = require('./data');
 const gqlRequest = require ('../gqlRequest')
 
 let respData = null
 let postData = null
+let userID = null
 
 describe('USER GET BY ID', () => {
     describe ('USER GET BY ID POSITIVE',()  => {
+
+        before('user create', (done) =>{
+            postData ={
+                query : userCreateQ,
+                variables : user
+
+            }
+            gqlRequest(postData)
+                .expect(200)
+                .end((err, res) => {
+                    if (err) return done(err)
+                    userID = res.body.data.userCreate._id
+                    console.log('userID =', userID)
+
+                    done()
+                })
+        })
+
         it ('user get by id', (done) =>{
             postData ={
                 query : userGetByIdQ,
-                variables : {
-                    userId: '66bc4d511490e9e8c7d9b65e'
+                variables :{userId : userID}
 }
-            }
+
             gqlRequest(postData)
                 .expect(200)
                 .end((err, res) => {
                     if (err) return done(err)
                     respData = res.body.data.userGetById
                     console.log(respData);
-                    expect(respData._id).eq('66bc4d511490e9e8c7d9b65e')
-
-       done()
+                    expect(respData._id).eq(userID)
+                    done()
     })
     })
 })
---
+++ src/_test/user3/data3.js
@@ -0,0 +1,8 @@
+const user3 = {
+    userInput: {
+        lastName: 'testLastName3', // Исправлено, чтобы соответствовать логике
+        firstName: 'testFirstName3'
+    }
+};
+
+module.exports = {user3};
